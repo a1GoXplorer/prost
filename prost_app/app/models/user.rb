db127326c92af0318ff761791551adfe950680fb
Mark Daniels <mark_daniels1982@hotmail.com>
2016-02-25 16:17:28 -0800

    add migration

--- prost_app/app/models/user.rb
+++ prost_app/app/models/user.rb
@@ -4,17 +4,15 @@ class User < ActiveRecord::Base
   BCrypt::Engine.cost = 12
 
 
-
-  attr_reader :password
+  #attr_reader :password 
 
   def password=(unencrypted_password)
     if unencrypted_password.nil?
       self.password_digest = nil
-    else
-       @password = unencrypted_password
-       self.password_digest = @BCrypt::Password.create(@password)
+    else 
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(@password)
     end
   end
 
-  
 end
